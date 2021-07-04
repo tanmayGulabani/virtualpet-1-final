d928b3292a8a49555138a4a227c337a6b53c2042
tanmayGulabani <75249050+tanmayGulabani@users.noreply.github.com>
2021-07-04 12:16:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var ball, database, position;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var hypnoticBallPosition = database.ref("ball/position");
+    hypnoticBallPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function readPosition(data) {
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function writePosition(x,y){
+    database.ref("ball/position").set({x:position.x+x, y: position.y+y});
+}
+
+function showError() {
+    console.log("Error in database");
+}
